05db98135ab5aeef8e976aed0910a042baacd03c
licichun <licichun@qq.com>
2018-01-15 19:40:38 +0800

    Add:User

--- Application/Home/Model/UserModel.class.php
+++ Application/Home/Model/UserModel.class.php
@@ -20,14 +20,26 @@ class UserModel extends Model
         $job_number = $param['job_number'];
         $mobile = $param['mobile'];
         $id_card = $param['id_card'];
+
         $result = array();
         $user = M('user')->where(['job_number' => $job_number])->find();
         if (empty($user)) {
             $result = array(
                 'msg_code' => '1001',
                 'msg' => '您输入的工号不正确',
-                'msg_en'=>'job_number'
+                'msg_en' => 'job_number'
             );
+            return $result;
+        }
+        if (preg_match("/^1[34578]{1}\d{9}$/", $param['mobile'])) {
+
+        } else {
+
+            $result = [
+                'msg_code' => '1002',
+                'msg' => '您输入的手机号码不正确',
+                'msg_en' => 'mobile'
+            ];
             return $result;
         }
         $user = M('user')->where(['mobile' => $mobile])->find();
@@ -35,7 +47,7 @@ class UserModel extends Model
             $result = array(
                 'msg_code' => '1002',
                 'msg' => '您输入的手机号码不正确',
-                'msg_en'=>'mobile'
+                'msg_en' => 'mobile'
             );
             return $result;
         }
@@ -44,25 +56,25 @@ class UserModel extends Model
             $result = array(
                 'msg_code' => '1003',
                 'msg' => '您输入的手机号码不正确',
-                'msg_en'=>'id_card'
+                'msg_en' => 'id_card'
             );
             return $result;
         }
-        $user = M('user')->where(['job_number' => $job_number,'mobile' => $mobile,'id_card' => $id_card])->find();
+        $user = M('user')->where(['job_number' => $job_number, 'mobile' => $mobile, 'id_card' => $id_card])->find();
         if (empty($user)) {
             $result = array(
                 'msg_code' => '1004',
                 'msg' => '您输入的手机号码不正确',
-                'msg_en'=>'not_result'
+                'msg_en' => 'not_result'
             );
             return $result;
-        }else{//登录成功
+        } else {//登录成功
             session('user_id', $user['id']);
             session('job_number', $user['job_number']);
             $result = array(
                 'msg_code' => '200',
                 'msg' => '您输入的手机号码不正确',
-                'msg_en'=>'id_card'
+                'msg_en' => 'id_card'
             );
         }
         return $result;
